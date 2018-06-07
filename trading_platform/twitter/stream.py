b6af2bb544086fb4a3bd3e77370ff6f31d23d3c4
skeller88 <skeller88@gmail.com>
2018-06-07 12:58:45 -0700

    install_requires

--- trading_platform/twitter/stream.py
+++ trading_platform/twitter/stream.py
@@ -1,7 +1,8 @@
-from trading_platform.twitter.auth import Auth
-from trading_platform.twitter.stream_listener import StreamListener
+import tweepy
 
 
-class Stream:
-    def __init__(self, auth: Auth, stream_listener: StreamListener):
-        self.stream = Stream(auth, stream_listener)
+class Stream(tweepy.Stream):
+    """
+    Bridge class. Invoke like so:
+    stream = Stream(auth=auth, listener=stream_listener)
+    """
