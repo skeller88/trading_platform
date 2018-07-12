c7a1e97949e10d6b5d82ea2a88fbb017a4176e6b
skeller88 <skeller88@gmail.com>
2018-07-11 17:26:31 -0700

    Package wheel

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="tradingplatform",
     # increment version according to https://semver.org/
-    version="1.3.5",
+    version="1.4.5",
     author="skeller88",
     author_email="skeller88@gmail.com",
     description="A trading system platform",
